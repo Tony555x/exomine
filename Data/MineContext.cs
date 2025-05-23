93f753d265bd4f7167d347798673c31ffa5520d2
Tony555x <antonkremov2007@gmail.com>
2025-05-23 22:19:31 +0300

    DBContext, UserGame composite key

--- Data/MineContext.cs
+++ Data/MineContext.cs
@@ -6,5 +6,12 @@ namespace exomine.Data
     public class MineContext(DbContextOptions options) : DbContext(options)
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<UserGame> UserGames { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserGame>().HasKey(ug => new { ug.UserId, ug.GameId });
+        }
     }
 }
