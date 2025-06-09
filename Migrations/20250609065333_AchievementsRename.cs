6d4f714ee077380e975e0aa5d2cde74e4d2a8e29
Tony555x <antonkremov2007@gmail.com>
2025-06-09 09:54:11 +0300

    More Renames

---
+++ Migrations/20250609065333_AchievementsRename.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace exomine.Migrations
+{
+    /// <inheritdoc />
+    public partial class AchievementsRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAchievement_Achievement_AchievementId",
+                table: "UserAchievement");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAchievement_Users_UserId",
+                table: "UserAchievement");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserAchievement",
+                table: "UserAchievement");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Achievement",
+                table: "Achievement");
+
+            migrationBuilder.RenameTable(
+                name: "UserAchievement",
+                newName: "UserAchievements");
+
+            migrationBuilder.RenameTable(
+                name: "Achievement",
+                newName: "Achievements");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserAchievement_AchievementId",
+                table: "UserAchievements",
+                newName: "IX_UserAchievements_AchievementId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserAchievements",
+                table: "UserAchievements",
+                columns: new[] { "UserId", "AchievementId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Achievements",
+                table: "Achievements",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAchievements_Achievements_AchievementId",
+                table: "UserAchievements",
+                column: "AchievementId",
+                principalTable: "Achievements",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAchievements_Users_UserId",
+                table: "UserAchievements",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAchievements_Achievements_AchievementId",
+                table: "UserAchievements");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAchievements_Users_UserId",
+                table: "UserAchievements");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserAchievements",
+                table: "UserAchievements");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Achievements",
+                table: "Achievements");
+
+            migrationBuilder.RenameTable(
+                name: "UserAchievements",
+                newName: "UserAchievement");
+
+            migrationBuilder.RenameTable(
+                name: "Achievements",
+                newName: "Achievement");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserAchievements_AchievementId",
+                table: "UserAchievement",
+                newName: "IX_UserAchievement_AchievementId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserAchievement",
+                table: "UserAchievement",
+                columns: new[] { "UserId", "AchievementId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Achievement",
+                table: "Achievement",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAchievement_Achievement_AchievementId",
+                table: "UserAchievement",
+                column: "AchievementId",
+                principalTable: "Achievement",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAchievement_Users_UserId",
+                table: "UserAchievement",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
