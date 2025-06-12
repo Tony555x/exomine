4695eb7d2ab2fe257dedf48b7b0f71e53646aa15
Tony555x <antonkremov2007@gmail.com>
2025-06-12 10:34:01 +0300

    admin countdown

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using exomine.Data.Models;
 using exomine.Models;
 using exomine.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace exomine.Controllers
 {
@@ -15,14 +16,20 @@ namespace exomine.Controllers
             _gen = gen;
         }
         [HttpGet]
-        public IActionResult AdminGenerate()
+        public async Task<IActionResult> AdminGenerate()
         {
+            int? userid = HttpContext.Session.GetInt32("UserId");
+            User? user = null;
+            if (userid != null) user = await _db.Users.Where(u => u.Id == userid).FirstOrDefaultAsync();
+            if (user == null) return RedirectToAction("Login", "Account");
+            if (user.Role != "Admin") return RedirectToAction("Index", "Home");
             return View(new AdminGenerateViewModel { Size = 5, Count = 10 });
         }
 
         [HttpPost]
         public async Task<IActionResult> AdminGenerate(AdminGenerateViewModel model)
         {
+            if (model.Size < 1) return View(model);
             Console.WriteLine(model.Size + " " + model.Type);
             List<Task> tasks = new();
             var games = await Task.WhenAll(
@@ -30,6 +37,7 @@ namespace exomine.Controllers
                     Task.Run(() =>
                     {
                         Game g=_gen.GenerateRandom(model.Size, model.Type);
+                        Console.WriteLine(i);
                         return g;
                     })
                 )
