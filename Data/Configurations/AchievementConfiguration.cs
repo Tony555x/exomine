d30f2cc81426372a73a948b6d61439bcebf050c2
Tony555x <antonkremov2007@gmail.com>
2025-06-12 10:58:04 +0300

    better achievements

--- Data/Configurations/AchievementConfiguration.cs
+++ Data/Configurations/AchievementConfiguration.cs
@@ -17,23 +17,23 @@ public class AchievementConfiguration:IEntityTypeConfiguration<Achievement>{
 
             new Achievement { Id = 3, Name = "Win a hexagon game at size 5", GridType = GridType.Hexagon, MinSize = 5 },
             new Achievement { Id = 4, Name = "Win a hexagon game at size 10", GridType = GridType.Hexagon, MinSize = 10 },
-            new Achievement { Id = 5, Name = "Win a hexagon game at difficulty at least 20", GridType = GridType.Hexagon, MinDifficulty = 20 },
-            new Achievement { Id = 6, Name = "Win a hexagon game at difficulty at least 20 under 30 seconds", GridType = GridType.Hexagon, MinDifficulty = 20, MaxTimeSeconds = 120 },
+            new Achievement { Id = 5, Name = "Win a hexagon game at difficulty at least 25", GridType = GridType.Hexagon, MinDifficulty = 20 },
+            new Achievement { Id = 6, Name = "Win a hexagon game at difficulty at least 25 under 60 seconds", GridType = GridType.Hexagon, MinDifficulty = 10, MaxTimeSeconds = 60 },
 
             new Achievement { Id = 7, Name = "Win a square game at size 5", GridType = GridType.Square, MinSize = 5 },
             new Achievement { Id = 8, Name = "Win a square game at size 10", GridType = GridType.Square, MinSize = 10 },
-            new Achievement { Id = 9, Name = "Win a square game at difficulty at least 20", GridType = GridType.Square, MinDifficulty = 20 },
-            new Achievement { Id = 10, Name = "Win a square game at difficulty at least 20 under 30 seconds", GridType = GridType.Square, MinDifficulty = 20, MaxTimeSeconds = 30 },
+            new Achievement { Id = 9, Name = "Win a square game at difficulty at least 25", GridType = GridType.Square, MinDifficulty = 25 },
+            new Achievement { Id = 10, Name = "Win a square game at difficulty at least 25 under 120 seconds", GridType = GridType.Square, MinDifficulty = 25, MaxTimeSeconds = 120 },
 
             new Achievement { Id = 11, Name = "Win a triangle game at size 5", GridType = GridType.Triangle, MinSize = 5 },
             new Achievement { Id = 12, Name = "Win a triangle game at size 10", GridType = GridType.Triangle, MinSize = 10 },
-            new Achievement { Id = 13, Name = "Win a triangle game at difficulty at least 20", GridType = GridType.Triangle, MinDifficulty = 20 },
-            new Achievement { Id = 14, Name = "Win a triangle game at difficulty at least 20 under 30 seconds", GridType = GridType.Triangle, MinDifficulty = 20, MaxTimeSeconds = 30 },
+            new Achievement { Id = 13, Name = "Win a triangle game at difficulty at least 30", GridType = GridType.Triangle, MinDifficulty = 30 },
+            new Achievement { Id = 14, Name = "Win a triangle game at difficulty at least 30 under 300 seconds", GridType = GridType.Triangle, MinDifficulty = 30, MaxTimeSeconds = 300 },
 
             new Achievement { Id = 15, Name = "Win a squareTriHex game at size 5", GridType = GridType.SquareTriHex, MinSize = 5 },
             new Achievement { Id = 16, Name = "Win a squareTriHex game at size 10", GridType = GridType.SquareTriHex, MinSize = 10 },
-            new Achievement { Id = 17, Name = "Win a squareTriHex game at difficulty at least 20", GridType = GridType.SquareTriHex, MinDifficulty = 20 },
-            new Achievement { Id = 18, Name = "Win a squareTriHex game at difficulty at least 20 under 30 seconds", GridType = GridType.SquareTriHex, MinDifficulty = 20, MaxTimeSeconds = 30 }
+            new Achievement { Id = 17, Name = "Win a squareTriHex game at difficulty at least 25", GridType = GridType.SquareTriHex, MinDifficulty = 25 },
+            new Achievement { Id = 18, Name = "Win a squareTriHex game at difficulty at least 25 under 180 seconds", GridType = GridType.SquareTriHex, MinDifficulty = 25, MaxTimeSeconds = 180 }
         ];
     }
 
---
+++ Migrations/20250612073603_ImproveAchievements.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace exomine.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImproveAchievements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "Win a hexagon game at difficulty at least 25");
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "MaxTimeSeconds", "MinDifficulty", "Name" },
+                values: new object[] { 60, 10, "Win a hexagon game at difficulty at least 25 under 60 seconds" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "MinDifficulty", "Name" },
+                values: new object[] { 25, "Win a square game at difficulty at least 25" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "MaxTimeSeconds", "MinDifficulty", "Name" },
+                values: new object[] { 120, 25, "Win a square game at difficulty at least 25 under 120 seconds" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 13,
+                columns: new[] { "MinDifficulty", "Name" },
+                values: new object[] { 30, "Win a triangle game at difficulty at least 30" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 14,
+                columns: new[] { "MaxTimeSeconds", "MinDifficulty", "Name" },
+                values: new object[] { 300, 30, "Win a triangle game at difficulty at least 30 under 300 seconds" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "MinDifficulty", "Name" },
+                values: new object[] { 25, "Win a squareTriHex game at difficulty at least 25" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "MaxTimeSeconds", "MinDifficulty", "Name" },
+                values: new object[] { 180, 25, "Win a squareTriHex game at difficulty at least 25 under 180 seconds" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "Win a hexagon game at difficulty at least 20");
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "MaxTimeSeconds", "MinDifficulty", "Name" },
+                values: new object[] { 120, 20, "Win a hexagon game at difficulty at least 20 under 30 seconds" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "MinDifficulty", "Name" },
+                values: new object[] { 20, "Win a square game at difficulty at least 20" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "MaxTimeSeconds", "MinDifficulty", "Name" },
+                values: new object[] { 30, 20, "Win a square game at difficulty at least 20 under 30 seconds" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 13,
+                columns: new[] { "MinDifficulty", "Name" },
+                values: new object[] { 20, "Win a triangle game at difficulty at least 20" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 14,
+                columns: new[] { "MaxTimeSeconds", "MinDifficulty", "Name" },
+                values: new object[] { 30, 20, "Win a triangle game at difficulty at least 20 under 30 seconds" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "MinDifficulty", "Name" },
+                values: new object[] { 20, "Win a squareTriHex game at difficulty at least 20" });
+
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "MaxTimeSeconds", "MinDifficulty", "Name" },
+                values: new object[] { 30, 20, "Win a squareTriHex game at difficulty at least 20 under 30 seconds" });
+        }
+    }
+}
